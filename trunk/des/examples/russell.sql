0069a6f39951c870d3faf3bd0bd8ce74d7f37395
Salcedonia <Salcedonia@e6e09dc5-25af-486e-56a6-9c3093488908>
2010-10-31 15:41:45 +0000

    Eliminados resources sobrantes para estandarizar el código fuente de forma que solo se actualice la carpete src.

--- trunk/des/examples/russell.sql
+++
@@ -1,6 +0,0 @@
-/sql
-create table man(man string);
-insert into man values ('barber');
-insert into man values ('mayor');
-create view shaves(man,to) as select 'barber',man from man,shaves where man.man=shaves.man except select * from shaves where man=to;
-select * from shaves;
